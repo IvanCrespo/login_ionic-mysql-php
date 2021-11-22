dfb6d6543bcbaa9c01ebd0b0c904471786be1b9c
Iván Crespo Reyes <79070074+IvanCrespo@users.noreply.github.com>
2021-11-22 16:56:53 -0600

    LocalStorage Login Save User

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PostProvider } from '../../providers/post-provider';
+import { ToastController } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +12,37 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  username: string;
+  anggota: any;
+  password: string;
+  id: number;
+
+
+  constructor(
+    private router: Router,
+    private postPvdr: PostProvider,
+    public toastCtrl: ToastController,
+    private storage: Storage
+  ) { }
+
+  ionViewWillEnter(){
+    this.storage.get('session_storage').then((res) => {
+      this.anggota = res;
+      this.id = this.anggota.id;
+      this.username = this.anggota.username;
+      this.password = this.anggota.password;
+    });
+  }
+
+  async Logout() {
+    this.storage.clear();
+    this.router.navigate(['/login']);
+    const toast = await this.toastCtrl.create({
+      message: 'Logout Successful',
+      duration: 2000
+    });
+    toast.present();
+  }
+
 
 }
